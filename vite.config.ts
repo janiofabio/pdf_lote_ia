c4a76c991a182f5e901ed92d98a9bfe86106c78b
janiofabio <35180272+janiofabio@users.noreply.github.com>
2024-04-12 12:44:05 -0300

    enviando segundo pacote

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/pdf_lote_ia",
   plugins: [react()],
-  base: "/pdf_lote_ia"
 });
